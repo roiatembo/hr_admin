1a1e9cfb7b0d7a5436e53670fb6ded33a4a0ec0a
Roia Tembo <roiatembo@gmail.com>
2023-10-01 18:50:28 +0200

    Added form validations

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,21 +8,35 @@ import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-// import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { FieldValues, useForm } from "react-hook-form";
+import { resolve } from "path";
 
 const defaultTheme = createTheme();
 
-export default function SignIn() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
+export default function Login() {
+  // const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
+  //   const data = new FormData(event.currentTarget);
+  //   console.log({
+  //     email: data.get("email"),
+  //     password: data.get("password"),
+  //   });
+  // };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+    getValues,
+  } = useForm();
+
+  const onSubmit = async (data: FieldValues) => {
+    //send to server
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    reset();
   };
 
   return (
@@ -43,31 +57,38 @@ export default function SignIn() {
           </Typography>
           <Box
             component="form"
-            onSubmit={handleSubmit}
+            onSubmit={handleSubmit(onSubmit)}
             noValidate
             sx={{ mt: 1 }}
           >
             <TextField
+              {...register("username", { required: "Username is required" })}
               margin="normal"
-              required
               fullWidth
-              id="email"
+              id="username"
               label="Username"
-              name="email"
-              autoComplete="email"
+              name="username"
+              autoComplete="username"
               autoFocus
             />
             <TextField
+              {...register("password", { required: "Password is required" })}
               margin="normal"
-              required
               fullWidth
               name="password"
               label="Password"
               type="password"
               id="password"
-              autoComplete="current-password"
+              autoComplete="password"
             />
+            {errors.username && (
+              <Typography>{`${errors.username.message}`}</Typography>
+            )}
+            {errors.password && (
+              <Typography>{`${errors.password.message}`}</Typography>
+            )}
             <Button
+              disabled={isSubmitting}
               type="submit"
               fullWidth
               variant="contained"
